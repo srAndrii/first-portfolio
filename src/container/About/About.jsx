8e6a992818550691ad209e192307205eab8ef536
Andrii <55akif5548@gmail.com>
2022-05-12 21:00:48 +0300

    Підключено Sanity

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -3,16 +3,19 @@ import { useEffect, useState } from 'react'
 import './About.scss'
 import { motion } from 'framer-motion'
 
+import { urlFor, client } from '../../client'
+
 import { images } from '../../constants'
 
-const abouts = [
-    {title:'Web Development', description:'I am a good web developer', imgUrl:images.about01},
-    {title:'Web Design', description:'I am a good web developer', imgUrl:images.about02},
-    {title:'UI/UX', description:'I am a good web developer', imgUrl:images.about03},
-    {title:'Web Animations', description:'I am a good web developer', imgUrl:images.about04},
-]
-
 const About = () => {
+    const [abouts, setAbouts]= useState([])
+
+    useEffect(() => {
+        const query = '*[_type == "abouts"]'
+        client.fetch(query)
+            .then((data) => setAbouts(data))
+    },[])
+
     return (
         <>
             <h2 className='head-text'>
@@ -28,7 +31,7 @@ const About = () => {
                         className="app__profile-item"
                         key={about.title + index}
                     >
-                        <img src={about.imgUrl} alt={about.title} />
+                        <img src={urlFor(about.imgUrl)} alt={about.title} />
                         <h2 className='bold-text' style={{marginTop:20}}>{about.title} </h2>
                         <p className='p-text' style={{marginTop:10}}>{about.description} </p>
                     </motion.div>
